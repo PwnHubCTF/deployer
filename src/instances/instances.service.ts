a5e3b200645a4ac97e4320cceb75737aa658b140
Eteckq <yohan.g99@gmail.com>
2023-01-24 11:30:47 +0100

    fix docker

--- src/instances/instances.service.ts
+++ src/instances/instances.service.ts
@@ -34,7 +34,7 @@ export class InstancesService {
        const inBuild = await this.buildQueue.getJobs(['active', 'waiting'])
        const inDestroy = await this.destroyQueue.getJobs(['active', 'waiting'])
        
-       return [...instances, ...[...inBuild, ...inDestroy].map(j => {return {...j.data, progress: j.progress()}})]
+       return [...instances.map(i => {return {...i, url: `${process.env.SERVER_URL}:${i.port}`}}), ...[...inBuild, ...inDestroy].map(j => {return {...j.data, progress: j.progress()}})]
     }
 
     async destroyInstance (owner: string) {
